863e7713eacdd1b323c174c8c10456a4983b6afb
JimLee12 <qkq010177@gmail.com>
2022-12-21 18:00:32 +0900

    22.12.21

---
+++ Python독학/HelloDjango/my_settings.py
@@ -0,0 +1,12 @@
+## 
+DATABASES = {
+  'default':{
+    'ENGINE':"django.db.backends.mysql",#1
+    'NAME':'test',#2
+    'USER':'root',#3
+    'PASSWORD':'tiger',
+    'HOST':'localhost',
+    'PORT':'3306',
+  } 
+}
+SECRET_KEY="django-insecure-4c)9@-_yc(x&v$fajqk#z@b!^bj-pjxb8)_x^42tw#a9rkb5q6"
